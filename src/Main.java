240fa2bfa9aa6ce8e7e187c2bdbb27203cf473c5
Sandun Silva <sandun.silva@debugsire.com>
2025-11-01 19:35:31 +0530

    update main class add arrayList

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Payment p1 = new CashOnDelivery(2500, "LKR", "123 Main Street");
-        p1.processPayment();
-        p1.generateReceipt();
-        System.out.println("-----------------------------------");
-
         Payment p2 = new BankTransfer(15000, "LKR", "ABC Bank", "1234567890", "REF2025");
-        p2.processPayment();
-        p2.generateReceipt();
-        System.out.println("-----------------------------------");
-
         Payment p3 = new CreditCardPayment(5000, "USD", "4111111111111111",
                 "John Doe", "12/27", 10000);
-        p3.processPayment();
-        p3.generateReceipt();
-        System.out.println("-----------------------------------");
-
         Payment p4 = new DebitCardPayment(3000, "USD", "5500000000000004",
                 "Jane Smith", "10/26", 4000);
-        p4.processPayment();
-        p4.generateReceipt();
-        System.out.println("-----------------------------------");
+
+        List<Payment> payments = new ArrayList<>();
+        payments.add(p1);
+        payments.add(p2);
+        payments.add(p3);
+        payments.add(p4);
+
+        // Runtime Polymorphism!
+        for (Payment payment : payments) {
+            payment.processPayment();
+            payment.generateReceipt();
+            System.out.println("-----------------------------------");
+        }
 
 
     }
